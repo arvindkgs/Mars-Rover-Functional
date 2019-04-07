535d628070fc1bce9fccb7d429fa817e74530959
akumargs <mail.arvind85@gmail.com>
2019-04-07 21:58:33 +0530

    Removed temporary variable

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Mars Rover Functional Programming
+
+I have solved the problem defined [HERE](https://github.com/arvindkgs/Mars-Rover-Traditional/blob/master/README.md) using functional programming.
+
+You can see how concise this code looks.
+
+I have used [fold](https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/fold.html) to reduce the the sequence of instructions to a single final position, which is the destination expected.
+
--- src/Rover.kt
+++ src/Rover.kt
@@ -1,12 +1,11 @@
 class Rover(var position: RoverPosition,val pl: Plateau){
     fun move(ins: String): RoverPosition{
-            position = ins.fold(position) { acc: RoverPosition, ins ->
-                when (ins) {
-                    'L' -> acc.left()
-                    'R' -> acc.right()
-                    else -> acc.move(pl)
-                }
+        return ins.fold(position) { acc: RoverPosition, ins ->
+            when (ins) {
+                'L' -> acc.left()
+                'R' -> acc.right()
+                else -> acc.move(pl)
             }
-        return position;
+        }
     }
 }
